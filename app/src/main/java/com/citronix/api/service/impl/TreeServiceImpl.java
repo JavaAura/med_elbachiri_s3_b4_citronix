fe25db0c17ea7a2d863fb82fdfd1b55024645cff
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-11-28 12:13:14 +0100

    CT-4-Tree-management

--- app/src/main/java/com/citronix/api/service/impl/TreeServiceImpl.java
+++ app/src/main/java/com/citronix/api/service/impl/TreeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.citronix.api.service.impl;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
